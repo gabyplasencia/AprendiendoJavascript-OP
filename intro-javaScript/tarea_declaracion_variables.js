4fdf29d7ee13c6e57db78fc281ca6744734e2c80
Gabriela <gabyplasencia1995@gmail.com>
2023-05-27 12:21:30 +0100

    Creando repositorio

---
+++ intro-javaScript/tarea_declaracion_variables.js
@@ -0,0 +1,16 @@
+const fecha_nacimiento = new Date("octuber 27 1995");
+
+const libro_favorito = {
+    titulo: "Orgullo y Prejucio",
+    autor: "Jane Austen",
+    "fecha-publicacion": new Date("january 18 1813"),
+    url: "https://www.agapea.com/Jane-Austen/Orgullo-y-prejuicio-9788491051329-i.htm"
+}
+const alumno = ["Gabriela", 27, true, fecha_nacimiento, libro_favorito];
+
+console.log(alumno);
+
+
+
+
+
