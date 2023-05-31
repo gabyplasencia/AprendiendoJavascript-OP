3fdb1a65070f47e944695d50f45d89d8552c4f26
Gabriela <gabyplasencia1995@gmail.com>
2023-05-31 12:14:30 +0100

    Ejercicios resueltos de numeros

---
+++ numeros/numeros.js
@@ -0,0 +1,22 @@
+//Una variable que contenga tu altura en centímetros (entero)
+let altura_cm = 175;
+
+//Una variable que contenga tu altura en metros (número de coma flotante)
+let altura_m = 1.75;
+
+// Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let peso_kg = 77.80;
+
+//Una variable que contenga tu altura en metros redondeada hacia arriba
+let alturaRedondeada = altura_m.toFixed(0);
+
+//Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let pesoRedondeado = parseInt(peso_kg);
+
+/*
+Una variable que contenga si "el máximo valor que se puede obtener en 
+Javascript + 1" es igual al máximo valor que se puede obtener en Javascript 
+*/
+
+
+console.log(pesoRedondeado);
