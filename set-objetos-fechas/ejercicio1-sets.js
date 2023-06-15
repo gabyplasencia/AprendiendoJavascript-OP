f0eb5579877fe563807275304442712d6c0ec4ba
Gabriela <gabyplasencia1995@gmail.com>
2023-06-15 16:41:39 +0100

    correcciones

--- set-objetos-fechas/ejercicio1-sets.js
+++ set-objetos-fechas/ejercicio1-sets.js
@@ -1,13 +1,14 @@
 //Un nuevo Set con los nombres de tu familia
-const familia = ["Mama", "Papa", "Hermana", "Hermano"];
-const familiaSet = new Set(familia);
+//const familia = ["Mama", "Papa", "Hermana", "Hermano"];
+//const familiaSet = new Set(familia);
+
+const familia = new Set(["Mama", "Papa", "Hermana", "Hermano"]);
 
 //Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
 familiaSet.add("yo");
 
 //Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
 familiaSet.add("Javascript");
-
 
 
 console.log(familiaSet);
--- set-objetos-fechas/ejercicio2-objetos.js
+++ set-objetos-fechas/ejercicio2-objetos.js
@@ -12,6 +12,7 @@ const misDatos = {
 const miEdad = misDatos.edad;
 
 //Una lista que contenga el objeto con tus datos personales y un nuevo objeto con los datos personales de tus dos mejores amig@s
+/*
 const datosElsy = {
     nombre: "Elsy",
     apellido: "Garcia",
@@ -28,6 +29,27 @@ const datosAri = {
 };
 
 const listaGente = [misDatos, datosElsy, datosAri];
+*/
+
+const listaGente = [
+    {
+        ...misdatos
+    },
+    {
+    nombre: "Elsy",
+    apellido: "Garcia",
+    edad: 28,
+    altura: 1.40,
+    eresDesarrollador: false
+    },
+    {
+    nombre: "Ariana",
+    apellido: "Collar",
+    edad: 26,
+    altura: 1.60,
+    eresDesarrollador: false
+    }
+]
 
 //Una nueva lista con los objetos de la lista anterior ordenados por edad, de mayor a menor
 listaGente.sort((a, b) => b.edad - a.edad)
