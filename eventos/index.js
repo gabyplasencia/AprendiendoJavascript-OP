f1dbe99cc9e9c5c03c4e8536bef92c294144b659
Gabriela <gabyplasencia1995@gmail.com>
2023-06-22 17:03:54 +0100

    ejercicio eventos

--- eventos/index.js
+++ eventos/index.js
@@ -1,1 +1,4 @@
-//Crea un fichero index.js
+//Vincula un evento de tipo "click" al botón anterior, que muestre una alerta en el navegador "click en el botón"
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", () => alert("Se ha hecho click"));
